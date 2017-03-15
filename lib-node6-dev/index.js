4864b9eb77f5a5c7d02144f0c51973c31fb9da69
Christophe Hurpeau <christophe@hurpeau.com>
2017-03-15 23:02:45 +0100

    feat: first commit

---
+++ lib-node6-dev/index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var _jsxFileName = 'index.jsx';
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _errorHtml = require('error-html');
+
+var _errorHtml2 = _interopRequireDefault(_errorHtml);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+const htmlRenderer = new _errorHtml2.default();
+
+exports.default = function index({ error }) {
+  return _react2.default.createElement('div', { dangerouslySetInnerHTML: { __html: htmlRenderer.render(error) }, __self: undefined,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 6
+    }
+  });
+};
+//# sourceMappingURL=index.js.map
